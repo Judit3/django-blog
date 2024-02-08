3bf9c40715ae68b1047f342e392002caed97a7ba
Judit3 <>
2024-02-08 10:20:27 +0000

    add class comment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://8000-judit3-django-blog-7bhfuaj2lq.us2.codeanyapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['8003-judit3-django-blog-7bhfuaj2lq.us2.codeanyapp.com', '.herokuapp.com']
 
 
 # Application definition
